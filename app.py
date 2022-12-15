be3151f33bb48bf3f7b70afbf968129e2a9ae205
Diego Coral <diegocoral@MacAir-Diego.local>
2022-12-15 11:56:18 -0300

    add comments

--- app.py
+++ app.py
@@ -2,24 +2,23 @@ from flask import Flask, render_template, request
 import random
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  # se crea inicialmente esta linea para conectar con inde.html
 def home():
-    
     return render_template('index.html')
 
-@app.route('/procesar', methods=['POST'])
+@app.route('/procesar', methods=['POST'])  #Se agrega esto para traer por POST datos de algo de html, es una entrada
 def procesar():
-    lista1 = request.form.get("lista")
-    lista1 = lista1.split()
-    lista2 = random.shuffle(lista1)
+    lista1 = request.form.get("lista") #trae la lista desde el formulario web desde index.html
+    lista1 = lista1.split()            #se divide el string anterior por espacios 
+    lista2 = random.shuffle(lista1)    #se hace aleatoria la lista
     print("la lista final es: ", lista1, "El tipo es:    ", type(lista1))
-    # Verificar paridad
+    # Verificar paridad para comprobar que la lista ingresada es par si no muestra error
     listapar = len(lista1)
     if listapar % 2 != 0:
         print("es par:",listapar)
         return render_template('index.html', listapar=listapar)
     else:
-        equipoblanco = lista1[0:len(lista1) // 2]
+        equipoblanco = lista1[0:len(lista1) // 2]   #se seleciiona el primer equipo desde la posicion 0 hasta la posicion de la mitad del tamaño de la lista
         equipooscuro = lista1[len(lista1) // 2:len(lista1)]
         print(equipoblanco, equipooscuro)
         return render_template('index.html', lista1=lista1, equipoblanco=equipoblanco, equipooscuro=equipooscuro)
